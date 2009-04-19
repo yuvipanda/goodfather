bbbcfa59c5c9a64773d9e7ddeb39a9c9628c4d53
Yuvi Panda <yuvipanda@thestatbot.com>
2009-04-19 12:52:09 +0530

    Re-used the same cursor, and realized read_streamed and read_all need to be different methods

--- Collectors/Python/gfpersist.py
+++ Collectors/Python/gfpersist.py
@@ -7,23 +7,24 @@ class Persistable:
 
 	def __maptype(self, value):
 		if type(value) is dict:
-			return Persistable(v)
+			return Persistable(value)
 		elif type(value) is list:
 			return [self.__maptype(v) for v in value]
 		else:
 			return value
 
-	def __init__(self, json):
-		for k, v in json.items():
-			self.__dict__[k] = self.__maptype(v)
+	def __init__(self, json=None):
+		if json:
+			for k, v in json.items():
+				self.__dict__[k] = self.__maptype(v)
 
 #Inherit from this for container - eg. Blog, TweetsSearch, etc
 class PersistanceContainer:
 
 	def persist(self, item):
-		self.connection.execute(
-				'INSERT INTO Data VALUES (:Data)', 
-				{'Data':jsonpickle.encode(item, unpickable=False)}
+		self.cursor.execute(
+				'INSERT INTO Data VALUES (?)', 
+				(jsonpickle.encode(item, unpickable=False),)
 				)
 		self.connection.commit()
 
@@ -32,21 +33,27 @@ class PersistanceContainer:
 		if createnew and fileexists:
 			os.remove(filepath) 
 		
-		self.connection=sqlite3.connect(filepath)
+		self.connection=sqlite3.connect(database=filepath)
+		self.cursor = self.connection.cursor()
 
 		if createnew: 
-			self.connection.execute(r'CREATE Table Data (Data)')
+			self.cursor.execute(r'CREATE Table Data (Data)')
 
 #Does a streaming Read. Use for large datasets.
 #Pro: Much less memory usage. Con: No in-memory caching
-def read_data(filepath, streaming=False):
-	conn = sqlite3.connect(filepath)
+def read_streaming(filepath, streaming=False):
+	conn = sqlite3.connect(database=filepath)
 	cur = conn.cursor()
 	cur.execute("SELECT * from Data")
-	if streaming:
-		while True:
-			row = cur.fetchone()
-			if row:			
-				yield Persistable(simplejson.loads(row[0]))
-	else:
-		return [Persistable(simplejson.loads(row[0])) for row in cur.fetchall()]
+	#if streaming:
+	while True:
+		row = cur.fetchone()
+		if row:			
+			yield Persistable(simplejson.loads(row[0]))
+
+#Does a all-at once Read. Loads all the objects into memory
+def read_all(filepath):
+	conn = sqlite3.connect(database=filepath)
+	cur = conn.cursor()
+	cur.execute("SELECT * from Data")
+	return [Persistable(simplejson.loads(row[0])) for row in cur.fetchall()]
