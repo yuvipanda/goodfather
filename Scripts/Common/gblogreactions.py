2db068ec86b179d2b0bde616bfd0879e8f6180ea
Yuvi Panda <yuvipanda@thestatbot.com>
2009-04-23 00:55:43 +0530

    Added a Common scripts section. Should probably end up in Python PATH. Added a Google Blog Reactions scraper

---
+++ Scripts/Common/gblogreactions.py
@@ -0,0 +1,19 @@
+import re
+import sys
+import locale
+from urllib2 import urlopen
+
+countRe = re.compile(r'of <b>(?P<count>(?:\d|,)+)</b>')
+
+#I need to do this for parsing comma seperated numbers. Sucks, I know. 
+#See here: http://mail.python.org/pipermail/python-list/2003-November/238093.html
+locale.setlocale(locale.LC_NUMERIC, "English") 
+
+def reactions_count(url):
+	searchUrl = "http://blogsearch.google.com/blogsearch?hl=en&ie=UTF-8&q=link:%s&filter=0" % url
+	text = urlopen(searchUrl).read()
+	result = re.findall(countRe, text)
+	if result:
+		return locale.atoi(re.findall(countRe, text)[0])
+	else:
+		return 0
