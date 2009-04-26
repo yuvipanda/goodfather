518e42936538297484a42783930cece01cc79798
Yuvi Panda <yuvipanda@thestatbot.com>
2009-04-26 22:48:17 +0530

    Removed dependency on locale module from googlesearch (py-core)

--- py-core/goodfather/util/googlesearch.py
+++ py-core/goodfather/util/googlesearch.py
@@ -1,19 +1,18 @@
 import re
-import locale
 from urllib2 import urlopen, Request
 
 countRe = re.compile(r'of (?:about )?<b>(?P<count>(?:\d|,)+)</b>')
 searchCountRe = re.compile(r'of about <b>(?P<count>(?:\d|,)+)</b>')
-#I need to do this for parsing comma seperated numbers. Sucks, I know. 
-#See here: http://mail.python.org/pipermail/python-list/2003-November/238093.html
-locale.setlocale(locale.LC_NUMERIC, "English") 
+
+def parse_int(string):
+	return int(string.replace(',',''))
 
 def _count_results(url):
 	req = Request(url, headers={'User-Agent':'GoodFather 0.9'})
 	text = urlopen(req).read()
 	results = re.findall(countRe, text)
 	if results:
-		return locale.atoi(results[0])
+		return parse_int(results[0])
 	else:
 		return 0
 
